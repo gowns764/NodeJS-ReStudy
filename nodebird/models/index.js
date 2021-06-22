9e2b47ab235821a51e8a93b272e9a08ae0594626
Raspbian <gowns764@naver.com>
2021-06-22 22:12:38 +0900

    Add files via upload

--- nodebird/models/index.js
+++ nodebird/models/index.js
@@ -1,12 +1,14 @@
 const Sequelize = require('sequelize');
-const env = process.env.NODE_ENN || 'development';
+const env = process.env.NODE_ENV || 'development';
 const config = require('../config/config')[env];
 const User = require('./user');
 const Post = require('./post');
 const Hashtag = require('./hashtag');
 
 const db = {};
-const sequelize = new Sequelize(config.database, config.username, config.passeord, config);
+const sequelize = new Sequelize(
+  config.database, config.username, config.password, config,
+);
 
 db.sequelize = sequelize;
 db.User = User;
